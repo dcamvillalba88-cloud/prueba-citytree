b247588903e1b70de4275ce6c21af906d570a0cc
Camilo Villalba <dcamvillalba88@gmail.com>
2026-01-11 20:33:56 -0500

    Pantalla administrador y mejora de pantalla ubicacion

--- prueba.citytree.js
+++ prueba.citytree.js
@@ -1,4 +1,14 @@
-// Home icon click event
-document.querySelector('.home-icon').addEventListener('click', function() {
-    window.location.reload(); // Reload the page as home action
-});
+// funciones de enlacess pantallas y Https
+function irApantallaInicio() {
+    window.location.href = "index.html"; // Navegar a la pagina principal
+}
+
+function irAMapaUbicacionEspecies() {
+    window.location.href = "ubicacion-especies.html"; // Navegar a la pantalla de mapa de ubicacion de especies
+
+}
+function irAGoogleMaps() {
+    window.open ("https://www.google.com/maps", "_blank"); // Navegar a Google Maps
+}
+
+
